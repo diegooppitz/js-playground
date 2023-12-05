953c24d25632d1d9e42f8a0739cbceaa8ee4cdc9
diegooppitz <diegooppitz@gmail.com>
2023-12-05 10:38:42 -0300

    some corrections and adjustments in the taxStates class and in the taxSystem class

--- tax-system/src/TaxStates/index.ts
+++ tax-system/src/TaxStates/index.ts
@@ -1,46 +1,46 @@
-import { ProductData, ErrorInfo } from "@/types";
+import { ProductData, TaxInfo } from "@/types";
 import { California } from "./States/California";
 import { NewYork } from "./States/NewYork";
 import { Ohio } from "./States/Ohio";
 import { IConcreteTaxRates } from "@/interfaces";
 
 export class TaxStates {
-    federalTax: number
-    taxRates: IConcreteTaxRates;
-    #stateFactoryMap: { [key: string]: (product: ProductData) => IConcreteTaxRates | string} = {
-        'california': this.#createCalifornia, 
-        // 'ohio': this.#createOhio,
-        // 'new-york': this.#createNewYork
+    taxInfo: TaxInfo;
+    #federalTax: number;
+    #stateFactoryMap: { [key: string]: (product: ProductData) => TaxInfo } = {
+        'california': (product) => this.#createCalifornia(product),
+        'ohio': (product) => this.#createOhio(product),
     };
 
     constructor(product: ProductData) {
-        this.federalTax = 5;
-        this.taxRates = { salesTax: 0, exciseTax: 0, federalTax: 5, totalTaxRate: 0 };
+        this.#federalTax = 5;
+        this.taxInfo = {};
         this.#getTaxRates(product);
     }
 
     #getTaxRates(product: ProductData) {
         const stateFactory = this.#stateFactoryMap[product.fiscalState.toLowerCase()];
-        if (product) stateFactory(product);
+        if (stateFactory) this.taxInfo = stateFactory(product) 
+        else this.taxInfo.error = "Error: Invalid fiscal state";
     }
 
-    #createCalifornia(product: ProductData) {
-        const california = new California(product, 5);
-        const stateTaxRates: IConcreteTaxRates | ErrorInfo = california.getTaxRates();
-    
-        console.log("error", 'error' in stateTaxRates)
+    #createCalifornia(product: ProductData): TaxInfo {
+        const california = new California(product, this.#federalTax);
+        const result = california.getTaxRates();
 
-        if ('error' in stateTaxRates) return stateTaxRates.error;
-        return stateTaxRates;
+        if (typeof result === 'string') {
+            return { error: result };
+        }
+        return { taxRates: result };
     }
-    
-    // #createOhio(product: ProductData) {
-    //     const ohio = new Ohio(product, 5); // colocar federal tax aqui
-    //     return ohio?.getTaxState() ?? this.#errorMsg;
-    // }
 
-    // #createNewYork(product: ProductData) {
-    //     const newYork = new NewYork(product, 5); // colocar federal tax aqui
-    //     return newYork?.getTaxState() ?? this.#errorMsg;
-    // }
+    #createOhio(product: ProductData): TaxInfo {
+        const ohio = new Ohio(product, this.#federalTax);
+        const result = ohio.getTaxRates();
+
+        if (typeof result === 'string') {
+            return { error: result };
+        }
+        return { taxRates: result };
+    }
 }
--- tax-system/src/TaxSystem/index.ts
+++ tax-system/src/TaxSystem/index.ts
@@ -7,7 +7,7 @@ export class TaxSystem {
 
     initSystem() {
         const californiaValidYear = { year: '2010', fiscalState: 'california', baseValue: 1000, productId: '5040' }
-        const ohioValidYear = { year: '2018', fiscalState: 'ohio', baseValue: 1000, productId: '5041' }
+        const ohioValidYear = { year: '2021', fiscalState: 'ohio', baseValue: 1000, productId: '5041' }
         const ohioInvalidYear = { year: '2015', fiscalState: 'ohio', baseValue: 1000, productId: '5042' }
         const invalidState = { year: '2015', fiscalState: 'californiaa', baseValue: 1000, productId: '5043' }
 
@@ -16,19 +16,29 @@ export class TaxSystem {
         product.createProduct(californiaValidYear)
         product.updateTotalValue(1200)
         const newProductData = product.getProduct();
-
         const taxStates = new TaxStates(newProductData);
-        const taxRates = taxStates // atualizar este retorno
-        console.log("Tax Rates", taxRates);
 
-        // // product 2
-        // const product2 = new Product();
-        // product2.createProduct(ohioValidYear)
-        // product2.updateTotalValue(1200)
-        // const newProductData2 = product2.getProduct();
+        if (taxStates.taxInfo.error) console.log("product1 -", taxStates.taxInfo.error);
+        else if (taxStates.taxInfo.taxRates) console.log("Product1 - Tax Rates:", taxStates.taxInfo.taxRates);
+        
+        // product 2
+        const product2 = new Product();
+        product2.createProduct(ohioValidYear)
+        product2.updateTotalValue(1200)
+        const newProductData2 = product2.getProduct();
+        const taxStates2 = new TaxStates(newProductData2);
 
-        // const taxStates2 = new TaxStates(newProductData2);
-        // const taxRates2 = taxStates2.taxRates
-        // console.log("Tax Rates 2", taxRates2);
+        if (taxStates2.taxInfo.error) console.log("product2 -", taxStates2.taxInfo.error);
+        else if (taxStates2.taxInfo.taxRates) console.log("Product 2 - Tax Rates:", taxStates2.taxInfo.taxRates);
+
+        // product 3 - invalid example
+        const product3 = new Product();
+        product2.createProduct(ohioInvalidYear)
+        product2.updateTotalValue(1200)
+        const newProductData3 = product3.getProduct();
+        const taxStates3 = new TaxStates(newProductData3);
+
+        if (taxStates3.taxInfo.error) console.log("product3 -", taxStates3.taxInfo.error);
+        else if (taxStates3.taxInfo.taxRates) console.log("Product 3 - Tax Rates:", taxStates3.taxInfo.taxRates);
     }
 }
