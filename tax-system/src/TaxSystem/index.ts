bf2f87745cc4e87650914154f9d39fc98a0bb717
diegooppitz <diegooppitz@gmail.com>
2023-12-06 10:44:00 -0300

    organize tax system code

--- tax-system/src/TaxSystem/index.ts
+++ tax-system/src/TaxSystem/index.ts
@@ -1,36 +1,24 @@
-import { Product } from "../Product";
-import { TaxStates } from "../TaxStates";
+import { generateProductData } from "../common"
+import { californiaValidYear, ohioInvalidYear, ohioValidYear, baseValue } from "../mock";
 
 export class TaxSystem {
     initSystem() {
-        const californiaValidYear = { year: '2010', fiscalState: 'california', baseValue: 1000, productId: '5040' }
-        const ohioValidYear = { year: '2021', fiscalState: 'ohio', baseValue: 1000, productId: '5041' }
-        const ohioInvalidYear = { year: '2015', fiscalState: 'ohio', baseValue: 1000, productId: '5042' }
-        const invalidState = { year: '2015', fiscalState: 'californiaa', baseValue: 1000, productId: '5043' }
-        const baseValue = 1200
-
          // product 1
-        const product = new Product(californiaValidYear, baseValue);
-        const newProductData = product.getProduct();
-        const taxStates = new TaxStates(newProductData);
+        const productTaxStates = generateProductData(californiaValidYear, baseValue)
 
-        if (taxStates.taxInfo.error) console.log("product1 -", taxStates.taxInfo.error);
-        else if (taxStates.taxInfo.taxRates) console.log("Product1 - Tax Rates:", taxStates.taxInfo.taxRates);
+        if (productTaxStates.taxInfo.error) console.log("product1 -", productTaxStates.taxInfo.error);
+        else if (productTaxStates.taxInfo.taxRates) console.log("Product1 - Tax Rates:", productTaxStates.taxInfo.taxRates);
         
         // product 2
-        const product2 = new Product(ohioValidYear, baseValue);
-        const newProductData2 = product2.getProduct();
-        const taxStates2 = new TaxStates(newProductData2);
+        const productTaxStates2 = generateProductData(ohioValidYear, baseValue)
 
-        if (taxStates2.taxInfo.error) console.log("product2 -", taxStates2.taxInfo.error);
-        else if (taxStates2.taxInfo.taxRates) console.log("Product 2 - Tax Rates:", taxStates2.taxInfo.taxRates);
+        if (productTaxStates2.taxInfo.error) console.log("product2 -", productTaxStates2.taxInfo.error);
+        else if (productTaxStates2.taxInfo.taxRates) console.log("Product 2 - Tax Rates:", productTaxStates2.taxInfo.taxRates);
 
         // product 3 - invalid example
-        const product3 = new Product(ohioInvalidYear, baseValue);
-        const newProductData3 = product3.getProduct();
-        const taxStates3 = new TaxStates(newProductData3);
+        const productTaxStates3 = generateProductData(ohioInvalidYear, baseValue)
 
-        if (taxStates3.taxInfo.error) console.log("product3 -", taxStates3.taxInfo.error);
-        else if (taxStates3.taxInfo.taxRates) console.log("Product 3 - Tax Rates:", taxStates3.taxInfo.taxRates);
+        if (productTaxStates3.taxInfo.error) console.log("product3 -", productTaxStates3.taxInfo.error);
+        else if (productTaxStates3.taxInfo.taxRates) console.log("Product 3 - Tax Rates:", productTaxStates3.taxInfo.taxRates);
     }
 }
