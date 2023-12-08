8bfdc495d6febfedb17a65a8b823d01d6fa54b97
diegooppitz <diegooppitz@gmail.com>
2023-12-08 11:20:24 -0300

    move some functions from california classe to common.ts file

---
+++ tax-system/src/TaxStates/States/Common.ts
@@ -0,0 +1,20 @@
+import { IConcreteTaxRates } from "@/interfaces";
+
+export const calcTaxRateForYear = (year: string, context: any) => {
+    const methodName: string = `year${year}`;
+
+    if (typeof (context)[methodName] === 'function') {
+        (context)[methodName]();
+        return '';
+    } else return "Error: Unregistered year";
+}
+
+export function calculateTotalTax(taxRates: IConcreteTaxRates): { totalTaxRate: number, errorMsg: string } {
+    taxRates.totalTaxRate = taxRates.salesTax + taxRates.exciseTax + taxRates.federalTax;
+    let errorMsg = '';
+
+    if (!taxRates.totalTaxRate) errorMsg = "Error: Calculate year tax rates error";
+    
+
+    return { totalTaxRate: taxRates.totalTaxRate, errorMsg };
+}
